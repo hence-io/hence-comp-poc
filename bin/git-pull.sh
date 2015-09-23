68ffc81f549b37e2120d41efb4d03b5727bcdb74
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-09-23 10:14:41 -0400

    - adding a git pull helper script - updating the gulp install to support sudo use when on non-windows machines

---
+++ bin/git-pull.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+cd components
+
+cd hence-card
+gpl
+cd ..
+
+#cd hence-event
+#gpl
+#cd ..
+#
+#cd hence-event-card
+#gpl
+#cd ..
+
+cd hence-product
+gpl
+cd ..
+
+cd hence-product-card
+gpl
+cd ..
+
+cd hence-user
+gpl
+cd ..
+
+cd hence-user-card
+gpl
+cd ..
+
+cd hence-user-biography
+gpl
+cd ..
+
+cd ..
+gpl
