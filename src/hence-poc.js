125f2d0add33dc58487a48f42173eadd6af4ed6b
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-09-25 23:02:15 -0400

    - continuing to expand on the cards modularity to support wider use cases - working on pragmatic samples of media content, albums, and flights

--- src/hence-poc.js
+++ src/hence-poc.js
@@ -9,6 +9,7 @@ import _sample from 'lodash/collection/sample';
 import _map from 'lodash/object/mapValues';
 
 import docs from './lib/docs';
+import buildCards from './lib/buildCards';
 
 /**
  * HencePoc Component
@@ -63,98 +64,17 @@ let HencePoc = Hence.Ui({
     _buildCards() {
     let {$, $$} = this;
 
-    // Some common defaults
-    let description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur assumenda commodi' +
-      ' cumque dolorem eaque eligendi id illum magnam nemo neque praesentium quam quas, saepe sequi soluta, tempore totam, voluptate?';
-
-    let action = (label = true)=> {
-      return {
-        label: label ? fake.commerce.product() : '',
-        icon: _sample(['apple', 'android', 'facebook', 'google', 'github', 'linux', 'rebel', 'steam', 'windows', '']),
-        float: _sample(['', 'left', 'right']),
-        action: (model, e)=> {
-          alert('Action activated!');
-        }
-      };
-    };
-
-    let image = 'http://lorempixel.com/250/150/nature';
-    let avatar = 'http://lorempixel.com/100/100/people';
-
     let target = {
       basic: $.basicCards,
       advanced: $.advancedCards,
       image: $.imageCards,
       user: $.userCards,
       product: $.productCards,
-      event: $.eventCards
+      event: $.eventCards,
+      useCase: $.userCaseCards
     };
 
-    //console.log('targets & docs', target, docs);
-
-    HenceCard.appendElementTo({
-      padded: true,
-      title: 'Padded example w/ actions',
-      subtitle: 'Padded example w/ actions',
-      actions: [action()],
-      image,
-      source: docs.advanced1()
-    }, target.advanced);
-
-    HenceCard.appendElementTo({
-      title: 'Example w/ actions, middle image',
-      subtitle: 'Example w/ actions, middle image',
-      actions: [action(false), action(false)],
-      image,
-      imagePosition: 'center',
-      description
-    }, target.advanced);
-
-    HenceCard.appendElementTo({
-      title: 'Example w/ actions & avatar',
-      subtitle: 'Example w/ actions',
-      actions: [action(), action(), action()],
-      actionsCentered: true,
-      image,
-      avatar,
-      description,
-      avatarShape: _sample(['square', 'circle'])
-    }, target.advanced);
-
-    HenceCard.appendElementTo({
-      title: 'Example w/ actions & avatar centered',
-      subtitle: 'Example w/ actions',
-      actions: [action(), action(), action()],
-      image,
-      avatar,
-      avatarPosition: 'center',
-      avatarShape: _sample(['square', 'circle'])
-    }, target.advanced);
-
-    HenceCard.appendElementTo({
-      title: 'Example w/ actions pre-opened, background image',
-      image,
-      imagePosition: 'background',
-      actions: [action(), action(), action()],
-      actionsPosition: 'center',
-      description
-    }, target.advanced);
-
-    HenceCard.appendElementTo({
-      title: 'Example w/ actions pre-opened, background image',
-      image,
-      imagePosition: 'background',
-      avatar,
-      avatarShape: _sample(['square', 'circle']),
-      actions: [action(), action(), action()],
-      description
-    }, target.advanced);
-
-    HenceCard.appendElementTo({
-      title: 'Icon Filled Actions',
-      actions: [action(false), action(false), action(false), action(false), action(false), action(false)],
-      description
-    }, target.advanced);
+    buildCards(target);
   }
 });
 
---
+++ src/lib/buildCards.js
@@ -0,0 +1,112 @@
+'use strict';
+/**
+ * @module hence-poc
+ */
+import fake from 'faker';
+
+import _sample from 'lodash/collection/sample';
+
+import docs from './docs';
+
+/**
+ * HencePoc Component
+ * @constructor
+ */
+let buildCards = (target)=> {
+  // Some common defaults
+  let description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur assumenda commodi' +
+    ' cumque dolorem eaque eligendi id illum magnam nemo neque praesentium quam quas, saepe sequi soluta, tempore totam, voluptate?';
+
+  let action = (label = true)=> {
+    return {
+      label: label ? fake.commerce.product() : '',
+      icon: _sample(['apple', 'android', 'facebook', 'google', 'github', 'linux', 'rebel', 'steam', 'windows', '']),
+      float: _sample(['', 'left', 'right']),
+      action: (model, e)=> {
+        alert('Action activated!');
+      }
+    };
+  };
+
+  let image = 'http://lorempixel.com/250/150/nature';
+  let avatar = 'http://lorempixel.com/100/100/people';
+
+  //console.log('targets & docs', target, docs);
+
+  HenceCard.appendElementTo({
+    padded: true,
+    title: 'Padded example w/ actions',
+    subtitle: 'Padded example w/ actions',
+    actions: [action()],
+    image,
+    source: docs.advanced1()
+  }, target.advanced);
+
+  HenceCard.appendElementTo({
+    title: 'Example w/ actions, middle image',
+    subtitle: 'Example w/ actions, middle image',
+    actions: [action(false), action(false)],
+    image,
+    imagePosition: 'center',
+    description
+  }, target.advanced);
+
+  HenceCard.appendElementTo({
+    title: 'Example w/ actions & avatar',
+    subtitle: 'Example w/ actions',
+    actions: [action(), action(), action()],
+    actionsCentered: true,
+    image,
+    avatar,
+    description,
+    avatarShape: _sample(['square', 'circle'])
+  }, target.advanced);
+
+  HenceCard.appendElementTo({
+    title: 'Example w/ actions & avatar centered',
+    subtitle: 'Example w/ actions',
+    actions: [action(), action(), action()],
+    image,
+    avatar,
+    avatarPosition: 'center',
+    avatarShape: _sample(['square', 'circle'])
+  }, target.advanced);
+
+  HenceCard.appendElementTo({
+    title: 'Example w/ actions pre-opened, background image',
+    image,
+    imagePosition: 'background',
+    actions: [action(), action(), action()],
+    actionsPosition: 'center',
+    description
+  }, target.advanced);
+
+  HenceCard.appendElementTo({
+    title: 'Example w/ actions pre-opened, background image',
+    image,
+    imagePosition: 'background',
+    avatar,
+    avatarShape: _sample(['square', 'circle']),
+    actions: [action(), action(), action()],
+    description
+  }, target.advanced);
+
+  HenceCard.appendElementTo({
+    title: 'Icon Filled Actions',
+    actions: [action(false), action(false), action(false), action(false), action(false), action(false)],
+    description
+  }, target.advanced);
+
+
+  // Travel
+  HenceCard.appendElementTo({
+    title: 'JFK <span>Icon</span> SFO',
+    image: 'https://ampersandtravel.files.wordpress.com/2012/09/maldives-dhoni-ampersand-travel.jpg'
+  }, target.useCase);
+
+  HenceCard.appendElementTo({
+    image: 'http://actualizingself.com/wp-content/uploads/2014/02/Travel-Hacking-221.jpg'
+  }, target.useCase);
+};
+
+export default buildCards;
